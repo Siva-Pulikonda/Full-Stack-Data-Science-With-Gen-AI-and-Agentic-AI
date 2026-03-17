4b9befe4176585385dc478ae422c0f7a712c815d
Siva-Pulikonda <sivapulikonda1995@gmail.com>
2026-03-17 15:00:24 +0530

    Add files via upload

---
+++ Run Python Code Using CLI ( Command Line Interpretor ).py
@@ -0,0 +1,12 @@
+# This code is a test to run python code in CLI( Command Line Interpretor )
+
+# Save this file with any name for example Test123.py
+# Open Command Prompt -> Change Directory to the File Location -> and run the Below Command 
+# >python Test123.py -> enter -> you will get the result
+ 
+a=5
+b=6
+c=7
+
+d=a+b+c
+
